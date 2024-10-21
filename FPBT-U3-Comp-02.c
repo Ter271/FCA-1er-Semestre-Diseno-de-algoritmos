32dee3514281f408305819cec661120ddc850ea6
Ter271 <89232405+Ter271@users.noreply.github.com>
2024-10-21 00:09:19 -0600

    Update and rename FPBT-U3-Comp-02.cpp to FPBT-U3-Comp-02.c

--- FPBT-U3-Comp-02.c
+++ FPBT-U3-Comp-02.c
@@ -1,11 +1,11 @@
 /*
 Nombre del programa: Volumen de cilindro
-Descripción: A partir del radio y altura, se calcula el volumen de un cilindro
+DescripciÃ³n: A partir del radio y altura, se calcula el volumen de un cilindro
 Variables: h -Altura del cilindro, r -Radio del cilindro, 
            r_cuadrada - Radio elevado al cuadrado, V -Volumen de cilindro
 Constantes: pi -Numero pi
-Fecha de creación: 28/Septiembre/2024
-Autor: Farfán Brenda
+Fecha de creaciÃ³n: 28/Septiembre/2024
+Autor: FarfÃ¡n Brenda
 */
 
 /*Se agregan librerias */
@@ -17,7 +17,7 @@ int main()
     float h, r, V=0, pi=3.1416; //Se definen variables y constantes
     
     /*Instrucciones al usuario */
-    printf("¡Bievenido a la calculadora para volumen de un cilindro!\nA continuacion, ingrese los datos solicitados:");
+    printf("Â¡Bievenido a la calculadora para volumen de un cilindro!\nA continuacion, ingrese los datos solicitados:");
 	printf("\nIngrese el valor del radio de la base del cilindro en centimetros (r): ");
     scanf("%f",&r);
     float r_cuadrada=pow(r,2); //Calculo del radio al cuadrado con funcion pow(base,exponente) y se define r_cuadrada
